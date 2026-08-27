2637f2b84597d758b571edc4c6a5402d2ce4a974
alok <alokmishra7464@gmail.com>
2026-08-27 17:36:32 +0530

    springboot initial project

---
+++ src/test/java/com/cinereserve/cine_reserve/CineReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinereserve.cine_reserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
